a3eef0aeb55f93b0cd4eb3fc2eaaa177f776000d
Zhiming Ni <1078830088@qq.com>
2021-06-04 15:40:45 +0800

    test fs.mkdir

---
+++ src/node-learning/createFolder.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+
+const folderName = 'new'
+fs.mkdir(`./${folderName}`,returnVal => {
+  if(returnVal) {
+    console.error(returnVal)
+  }else {
+    console.log(`A new folder "${folderName}" has been generated sucessfully!`)
+  }
+})
