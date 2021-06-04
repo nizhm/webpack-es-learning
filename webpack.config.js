69377c03c3116bf5dd53e89f26c79c6771686ec8
Zhiming Ni <1078830088@qq.com>
2021-06-04 13:06:53 +0800

    create webpack project

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, 'build')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "ECMAScript"
+    })
+  ],
+  devServer: {
+    contentBase: 'build',
+    host: '192.168.1.150',
+    port: 9000,
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(csv|tsv)/i,
+        use: 'csv-loader'
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2|otf)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  }
+}
