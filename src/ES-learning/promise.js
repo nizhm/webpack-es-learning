b687b6b94ef2758a76b20b8873a471aba536d5d7
Zhiming Ni <1078830088@qq.com>
2021-06-16 18:04:56 +0800

    learn promise

--- src/ES-learning/promise.js
+++ src/ES-learning/promise.js
@@ -1,1 +1,20 @@
-setTimeout(console.log, 500, new Promise(() => {}))
+const showResult = () => {
+  return new Promise((fn1, fn2) => {
+    const done = true
+    if(done) {
+      const workFulfilled = {code: 200, data: {status: 'workFulfilled'}, msg: 'success'}
+      fn1(workFulfilled)
+    }else {
+      const workRejected = new Error('workRejected')
+      fn2(workRejected)
+    }
+  })
+}
+showResult().then(res => {
+  console.log(res)
+  return Promise.resolve(res.code)
+}).then(val => {
+  console.log(val)
+}).catch(err => {
+  console.log(err)
+})
