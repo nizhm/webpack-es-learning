ff805a6396a485936e3b6f672313083784140cde
Zhiming Ni <1078830088@qq.com>
2021-06-17 14:51:49 +0800

    learn promise

---
+++ src/es-learning/promise.js
@@ -0,0 +1,23 @@
+const showResult = () => {
+  return new Promise((resolve, reject) => {
+      resolve('success')
+})
+}
+let resultPromise = showResult().then(
+    value => {
+    console.log('value:')
+throw new Error(value)
+},
+reason => {
+  console.log('reason:')
+  console.log(reason)
+}
+).catch(err => {
+  console.log('catch:')
+console.log(err)
+return new Error()
+})
+setTimeout(()=>{
+  console.log(resultPromise)
+},5000)
+console.log(Promise.resolve(new Error()))
