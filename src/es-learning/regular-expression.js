5d4f0510889cfc14a18fbf68ed8300162f7a90f3
Zhiming Ni <1078830088@qq.com>
2021-08-23 18:36:03 +0800

    learn RegExp

---
+++ src/es-learning/regular-expression.js
@@ -0,0 +1,56 @@
+/**
+ * @RegularExpression
+ * @Object %RegExpPrototype%
+ *    1.constructor  %RegExp%
+ *    2.source       pattern string [[OriginalSource]]
+ *    3.flags        sgimyu  [[OriginalFlags]]
+ *    4.dotAll       's' => true (ES10)
+ *    5.global       'g' => true
+ *    6.ignoreCase   'i' => true
+ *    7.multiline    'm' => true
+ *    8.sticky       'y' => true
+ *    9.unicode      'u' => true
+ *    10.exec(string)
+ *      10.1 返回值是类数组或者null
+ *      10.2 [
+ *        '[match result string]',
+ *        index: { Integer }, // 匹配结果的开始位置（index + result.length = lastIndex）
+ *        input: { String }, // 传入的字符串
+ *        groups:
+ *       ]
+ *    11.test(string) test内部调用exec并判断其返回值，不为null则返回true，否则返回false；
+ *    12.compile
+ *    13.toString
+ *      13.1 regExp.valueOf() == regExp.toString() => true
+ *      13.2 regExp.valueOf() === regExp.toString() => false(Object do not equal to String)
+ * @Object %RegExpInstance%
+ *    1.lastIndex
+ *      1.1 lastIndex初始值为0；
+ *      1.2 lastIndex决定下次匹配的开始位置（where to start the next match）;
+ *      1.3 exec()和test()方法会用到（test()内部会调用exec()并判断其返回值是否为null）；
+ *        1.3.1 global模式下，每配对到一次，返回{ Array } | true，lastIndex记录下次匹配开始位置；
+ *        1.3.2 global模式下，未匹配到，返回null | false，lastIndex重置为0；
+ *        1.3.3 非global模式下，以上两种情况，lastIndex都不会变，一直是0；
+ */
+
+function printRegExp(proto, regExpObj) {
+  const protoKeyArr = Object.getOwnPropertyNames(proto)
+  const objKeyArr = Object.getOwnPropertyNames(regExpObj)
+  const keyArr = [...protoKeyArr, ...objKeyArr]
+  for(const item of keyArr) {
+    console.log(`${ item }: ${ regExpObj[item] }`)
+  }
+}
+const regExp = new RegExp('[1-9\-]{2,3}', 'gi')
+const str = '01023045601110'
+const str2 = '07890222204560'
+console.log(regExp.valueOf() == regExp.toString())
+console.log(regExp.lastIndex)
+console.log(regExp.exec(str))
+console.log(regExp.lastIndex)
+console.log(regExp.test(str2))
+console.log(regExp.lastIndex)
+console.log(regExp.test(str))
+console.log(regExp.lastIndex)
+console.log(regExp.test(str))
+console.log(regExp.lastIndex)
