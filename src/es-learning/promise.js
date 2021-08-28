41c6bd52336fef1011c2b93c2c01827e6190d521
Zhiming Ni <1078830088@qq.com>
2021-08-28 13:43:24 +0800

    learn promise

--- src/es-learning/promise.js
+++ src/es-learning/promise.js
@@ -1,23 +1,36 @@
-const showResult = () => {
-  return new Promise((resolve, reject) => {
-      resolve('success')
+//const showResult = () => {
+//  return new Promise((resolve, reject) => {
+//      resolve('success')
+//})
+//}
+//let resultPromise = showResult().then(
+//    value => {
+//    console.log('value:')
+//throw new Error(value)
+//},
+//reason => {
+//  console.log('reason:')
+//  console.log(reason)
+//}
+//).catch(err => {
+//  console.log('catch:')
+//console.log(err)
+//return new Error()
+//})
+//setTimeout(()=>{
+//  console.log(resultPromise)
+//},5000)
+//console.log(Promise.resolve(new Error()))
+const promise = new Promise(resolve => {
+  setTimeout(() => {
+    resolve('111')
+  }, 1000)
 })
-}
-let resultPromise = showResult().then(
-    value => {
-    console.log('value:')
-throw new Error(value)
-},
-reason => {
-  console.log('reason:')
-  console.log(reason)
-}
-).catch(err => {
-  console.log('catch:')
-console.log(err)
-return new Error()
+promise.then(res => {
+  console.log('then:' + res)
+  console.log(val)
+}).catch(err => {
+  console.log('error:' + err)
+}).finally(() => {
+  console.log('finally:')
 })
-setTimeout(()=>{
-  console.log(resultPromise)
-},5000)
-console.log(Promise.resolve(new Error()))
