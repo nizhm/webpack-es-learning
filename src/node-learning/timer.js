aaf8a870fad2a21289cb235a4ca0a720f7d54b1e
Zhiming Ni <1078830088@qq.com>
2021-09-13 15:22:32 +0800

    learn timer

---
+++ src/node-learning/timer.js
@@ -0,0 +1,16 @@
+let startTime = 0;
+let endTime = 0;
+function timer() {
+  startTime = Date.now();
+  setTimeout(() => {
+    endTime = Date.now();
+  }, 1000);
+  for(let i = 0; i <= 1000000; i++) {
+    console.log('loop: ', i);
+  }
+}
+timer();
+setTimeout(() => {
+  console.log(`endTime(${ endTime }) - startTime(${ startTime }): ${ endTime - startTime }`);
+  console.log(`System waiting time: ${ endTime - startTime - 1000 }ms`);
+}, 2000);
