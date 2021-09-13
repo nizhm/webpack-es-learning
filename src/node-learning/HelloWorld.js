855217e8da1e0eb286a843c650f22730f04f27ff
Zhiming Ni <1078830088@qq.com>
2021-09-13 15:35:27 +0800

    normalize HelloWorld

--- src/node-learning/HelloWorld.js
+++ src/node-learning/HelloWorld.js
@@ -1,20 +1,21 @@
+import readline from 'readline';
 const printHelloWorld = function (num) {
-  for(let i =0;i<num;i++){
-    if(i==0) {
-      console.log("Let's start:")
+  for(let i = 0; i < num; i++){
+    if(i === 0) {
+      console.log("Let's start:");
     }
-    console.log(i+': Hello World, Javascript!')
-    if(i==9) {
-      console.log("Have finished!")
+    console.log(i+': Hello World, Javascript!');
+    if(i === 9) {
+      console.log("Have finished!");
     }
   }
 }
-const readline = require('readline').createInterface({
+const readliner = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-})
-readline.question('Enter a number:',num => {
-  readline.close()
-  printHelloWorld(Number(num))
-  console.error(new Error('error message'))
-})
+});
+readliner.question('Enter a number:', num => {
+  readliner.close();
+  printHelloWorld(Number(num));
+  //console.error(new Error('error message'));
+});
