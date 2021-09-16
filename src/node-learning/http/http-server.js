9022894be23f6b95a02df3f148a7aabb8cc10848
Zhiming Ni <1078830088@qq.com>
2021-09-16 17:49:06 +0800

    learn http

--- src/node-learning/http/http-server.js
+++ src/node-learning/http/http-server.js
@@ -1,12 +1,14 @@
-const http = require('http')
-const hostname = '127.0.0.1'
-const port = 8888
-const server = http.createServer((req, res) => {
-  console.log(process.env.NODE_ENV)
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World Http Server!')
-})
+const http = require('http');
+
+
+const hostname = '127.0.0.1';
+const port = 8888;
+const server = http.createServer(function (req, res){
+  console.log(arguments);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World Http Server!');
+});
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${ hostname }:${ port }`)
-})
+  console.log(`Server running at http://${ hostname }:${ port }`);
+});
