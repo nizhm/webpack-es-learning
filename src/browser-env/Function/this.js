f9ecff477c6a9d9633a6b61a61eae5a1b8390698
nzhming <1078830088@qq.com>
2022-01-22 17:13:50 +0800

    learn scope

---
+++ src/browser-env/Function/this.js
@@ -0,0 +1,25 @@
+debugger;
+function funC() {
+  const position = 'C'
+  console.log('funC')
+  console.log(this)
+}
+function funB() {
+  const position = 'B'
+  console.log('funB')
+  console.log(this)
+  funC()
+}
+function funA() {
+  'use strict'
+  // debugger;
+  const position = 'A'
+  console.log('funA')
+  console.log(this)
+  funB()
+}
+let obj = {
+  name: 'nizhm',
+  fn: funA
+}
+funA()
