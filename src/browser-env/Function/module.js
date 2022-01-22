ff2588447f52b641851b3253f036bbdb891060d7
nzhming <1078830088@qq.com>
2022-01-22 17:13:33 +0800

    learn scope

---
+++ src/browser-env/Function/module.js
@@ -0,0 +1,14 @@
+// import LexicalEnvironmentOperation from '../../es-learning/lexical-environment/LexicalEnvironmentOperation'
+debugger;
+function printInfo(x, y, z) {
+  console.log(x + y + z)
+}
+
+let x = 'x'
+let y = 'y'
+var zzz = 'z'
+
+printInfo(x, y, zzz)
+
+// const o = new LexicalEnvironmentOperation()
+// console.log(o)
