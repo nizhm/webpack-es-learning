cafc58df1a2af1444da6f3ee5e90561abb31f700
nzhming <1078830088@qq.com>
2022-03-28 16:04:01 +0800

    learn build

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: "production",
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, 'build')
