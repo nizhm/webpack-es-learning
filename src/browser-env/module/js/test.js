f0da0c3b63d5df712c45ad43336527d5e8fa6e5e
nzhming <1078830088@qq.com>
2022-03-29 18:33:56 +0800

    learn browser environment module

---
+++ src/browser-env/module/js/test.js
@@ -0,0 +1,7 @@
+import { moduleName as moduleNameA } from './constants.js'
+
+function showModuleName() {
+  const moduleFileName = document.getElementById('moduleFileName').textContent
+  console.log(moduleNameA)
+  console.log(moduleFileName)
+}
