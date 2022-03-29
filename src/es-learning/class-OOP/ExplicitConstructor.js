53db23bb933d783f59fb2d4c475fc067b0d6bbd9
nzhming <1078830088@qq.com>
2022-03-29 18:34:18 +0800

    learn module

--- src/es-learning/class-OOP/ExplicitConstructor.js
+++ src/es-learning/class-OOP/ExplicitConstructor.js
@@ -1,14 +1,33 @@
 class ExplicitConstructor {
   static createTime = Date.now()
+  static {
+    // `this` refer to Class
+    console.log(typeof this)
+  }
 
   constructor(name = 'nizhm', age = 25) {
+    // `this` refer to a new object
+    console.log(this)
     this.n = name
     this.age = age
+    this.classPropertyA = 'classPropertyA'
+    this.classPropertyB = 'classPropertyB'
+
+    let constructorPropertyA = 'constructor place variable'
+    const constructorPropertyB = 'constructor site variable'
+    function logInfo() {
+      console.log(constructorPropertyA)
+      console.log(constructorPropertyB)
+    }
   }
 
   printProfile() {
     console.log(`${this.n} is ${this.age} years old.`)
   }
 }
+
+let obj = new ExplicitConstructor()
+console.log(obj)
+console.log(Object.getPrototypeOf(new Promise(resolve => resolve())))
 
 module.exports = ExplicitConstructor
--- src/es-learning/class-OOP/index.js
+++ src/es-learning/class-OOP/index.js
@@ -1,19 +1,23 @@
 // CommonJS module
 const Logger = require('./Logger')
-const ExplicitConstructor = require('./ExplicitConstructor')
+// const ExplicitConstructor = await import('./ExplicitConstructor.js')
+import EventEmitter from 'events'
 
 // ES module
 // import Logger from './Logger.js'
 
 // Logger.error('There is an error')
 
-// const explicitConstructor = new ExplicitConstructor('ni', 26)
-class SubExplicitConstructor extends ExplicitConstructor {
-  static createTime = new Date()
-  constructor() {
-    super(...arguments)
-  }
-}
-console.log(SubExplicitConstructor.createTime)
-const subExplicitConstructor = new SubExplicitConstructor('ni')
-subExplicitConstructor.printProfile()
+// module object of node
+// console.log(module)
+// class SubExplicitConstructor extends ExplicitConstructor {
+//   static createTime = new Date()
+//   constructor() {
+//     super(...arguments)
+//   }
+// }
+// console.log(SubExplicitConstructor.createTime)
+// const subExplicitConstructor = new SubExplicitConstructor('ni')
+// subExplicitConstructor.printProfile()
+const e = new EventEmitter()
+console.log(e)
