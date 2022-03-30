30b4440f252bccd5422861d1d7649584342b6a41
nzhming <1078830088@qq.com>
2022-03-30 15:29:07 +0800

    learn function name of Function Expression

--- src/browser-env/Function/js/FunctionNameOfFunctionExpression.js
+++ src/browser-env/Function/js/FunctionNameOfFunctionExpression.js
@@ -1,9 +1,10 @@
 let sayHello = function sayHello(name) {
   // 'use strict'
   /**
-   * 1.function name of a Function Expression is local only to the function body scope.
-   * 2.it is a constant variable which ignores any assignment,
+   * 1.Function name of a Function Expression is local only to the function body scope.
+   * 2.It is a constant variable which ignores any assignment,
    * and an Error{@TypeError} will be reported by browser when assign to it in strict mode.
+   * Additionally, function name of a Function Declaration is not a constant variable.
    * 3.this feature can be used for invoked self inside function body.
    * tips: `arguments.callee` is a non-standard feature
    * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/function#named_function_expression
