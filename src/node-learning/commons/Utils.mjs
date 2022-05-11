27bac89b46f1e0f39f57e2c0b133349fdd8b7781
Zhiming Ni <1078830088@qq.com>
2022-05-11 22:32:01 +0800

    add getChainOf util;

--- src/node-learning/commons/Utils.mjs
+++ src/node-learning/commons/Utils.mjs
@@ -1,4 +1,4 @@
-export function getRangeOf(typedArray) {
+function getRangeOf(typedArray) {
   if (!typedArray) throw 'A typedArray should be specified, for example "Int8Array".';
 
   if (!(typedArray.__proto__ && (typedArray.__proto__ === Int8Array.prototype.__proto__.constructor))) throw `${typedArray} is not type of TypedArray.`;
@@ -34,3 +34,44 @@ export function getRangeOf(typedArray) {
   return [lowerBoundary, upperBoundary];
 
 }
+
+/**
+ * Get the prototype chain until `null`
+ *
+ * @param {Object|Function} obj - the object
+ * @return {*[]|undefined}
+ */
+
+const getChainOf = function getChainOf(obj) {
+  // invoked by [[Call]] only
+  if (this instanceof getChainOf) {
+    throw new TypeError(`function 'getChainOf' is not a constructor`)
+  }
+
+  // wrapper types, object type and function type are available
+  if (obj === undefined || obj === null) {
+    throw new TypeError(`${obj} is not a type of {boolean|number|string|symbol|function|object}`)
+  }
+
+  const prototypeChain = []
+  try {
+    const getProto = function getProto(o) {
+      const proto = Object.getPrototypeOf(o)
+      prototypeChain.push(proto)
+
+      // retrieve next prototype object
+      if (proto !== null) {
+        getProto(proto)
+      }
+    }
+    getProto(obj)
+  } catch(e) {
+    console.error(e)
+    return undefined
+  }
+
+  return prototypeChain
+}
+
+export { getRangeOf, getChainOf }
+
