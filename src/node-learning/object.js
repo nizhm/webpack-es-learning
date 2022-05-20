912bf1fcad806c3f9d1d06ba82f744f6b75844e4
nzhming <1078830088@qq.com>
2022-05-20 17:51:47 +0800

    learn server module;

--- src/node-learning/object.js
+++ src/node-learning/object.js
@@ -1,14 +1,24 @@
 import { objectGetOwnPropertyDescriptors, objectGetOwnPropertyNames } from './commons/Object.mjs';
 import { defTag, getChainOf } from './commons/Utils.mjs';
 import EventEmitter from 'events';
-import { createServer, Server, Agent } from 'http';
+import { createServer, Server, Agent, IncomingMessage, ServerResponse } from 'http';
 import net, { Socket,Stream } from 'net';
 import stream, { Readable, Writable, Duplex, Transform } from 'stream';
 import buffer from 'buffer';
-import path from 'src/node-learning/path/path.js';
+// import path from 'src/node-learning/path/path.js';
 
+defTag(Object.prototype, 'Object');
+const server = createServer((request, response) => {
+  console.log(objectGetOwnPropertyDescriptors(response));
+  console.log(objectGetOwnPropertyNames(objectGetOwnPropertyDescriptors(response)).join('\n'));
+  console.log(getChainOf(response));
+  response.end('ok');
+});
+server.listen(8080, '127.0.0.1', () => {
+  console.log(`Server running at http://192.168.1.150:8080`);
+});
 
-// defTag(Object.prototype, 'Object');
-// console.log(objectGetOwnPropertyDescriptors(path));
-// console.log(objectGetOwnPropertyNames(objectGetOwnPropertyDescriptors(path)).join('\n'));
-// console.log(getChainOf(path));
+// console.log(IncomingMessage.prototype.__proto__ === Readable.prototype);
+// console.log(objectGetOwnPropertyDescriptors(ServerResponse));
+// console.log(objectGetOwnPropertyNames(objectGetOwnPropertyDescriptors(ServerResponse)).join('\n'));
+// console.log(getChainOf(ServerResponse));
